bbab4edf4b6ac6d45906288d4bd1309f5cd6ac30
Josh Russell <jrussell@hubspot.com>
2013-01-21 11:08:45 -0500

    fileserver define

---
+++ manifests/fileserver.pp
@@ -0,0 +1,17 @@
+class puppet::fileserver {
+
+  include ::concat::setup
+
+  ::concat { '/etc/puppet/fileserver.conf':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    notify => Service[$::puppet::master::service_notify];
+  }
+
+  ::concat::fragment { 'puppet_fileserver_conf_header':
+    target  => '/etc/puppet/fileserver.conf',
+    order   => 00,
+    source => 'puppet:///modules/puppet/fileserver-header.conf',
+  }
+}
